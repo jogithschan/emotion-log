c86aabb4d26f7f1112b88582aea18caf2b236fe8
jogithschan <jogith2705@gmail.com>
2023-11-25 01:00:45 +0530

    emotion cards 1

--- constants/images.js
+++ constants/images.js
@@ -1,5 +1,12 @@
 import profile from "../assets/images/kemal.jpg";
+import e1 from "../assets/images/e1.jpg";
+import e2 from "../assets/images/e2.jpg";
+import e3 from "../assets/images/e3.jpg";
+import e4 from "../assets/images/e4.jpg";
+import e5 from "../assets/images/e5.jpg";
+
+
 
 export default {
-  profile,
+  profile, e1, e2, e3, e4, e5
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, FlatList, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image, Dimensions } from 'react-native';
 import { firebase, auth, firestore } from '../firebase/firebase'
 import { useNavigation } from '@react-navigation/native';
+import { COLORS, FONT, SHADOWS, SIZES, images } from "../constants"
+
+const screenWidth = Dimensions.get('window').width;
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -32,15 +35,44 @@ const HomeScreen = () => {
         )
 }, [])
 
-  const renderEntity = ({ item }) => {
-    return (
-      <TouchableOpacity style={styles.cardContainer}>
-        <Text>Date: {item.timestamp ? item.timestamp.toDate().toLocaleDateString() : 'N/A'}</Text>
-        <Text>Time: {item.timestamp ? item.timestamp.toDate().toLocaleTimeString() : 'N/A'}</Text>
-        <Text>Response: {item.responseZero}</Text>
-      </TouchableOpacity>
-    );
-  };
+const renderEntity = ({ item }) => {
+  let imageUri = '';
+
+  if (item.responseZero >= -3 && item.responseZero < -1) {
+    imageUri = images.e1;
+  } else if (item.responseZero >= -1 && item.responseZero < 0) {
+    imageUri = images.e2;
+  } else if (item.responseZero === 0) {
+    imageUri = images.e3;
+  } else if (item.responseZero > 0 && item.responseZero <= 1) {
+    imageUri = images.e4;
+  } else if (item.responseZero > 1 && item.responseZero <= 3) {
+    imageUri = images.e5;
+  }
+
+  return (
+    <TouchableOpacity style={styles.cardContainer}>
+      <View style={styles.iconContainer}>
+        {/* Example: Replace 'imageUri' with the correct logic */}
+        <Image
+          source={imageUri} // Make sure 'imageUri' has the correct image file name
+          resizeMode="contain"
+          style={styles.iconImage}
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textFormat}>
+          Date: {item.timestamp ? item.timestamp.toDate().toLocaleDateString() : 'N/A'}
+        </Text>
+        <Text style={styles.textFormat}>
+          Time: {item.timestamp ? item.timestamp.toDate().toLocaleTimeString() : 'N/A'}
+        </Text>
+        {/* Other text components can be added here */}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
 
   return (
     <View style={styles.container}>
@@ -57,6 +89,7 @@ const HomeScreen = () => {
         data={entities}
         renderItem={renderEntity}
         keyExtractor={(item) => item.id}
+        contentContainerStyle={{ flexGrow: 1, paddingBottom: 100 }}
         style={styles.flatList}
       />
       </View> 
@@ -95,37 +128,36 @@ const styles = StyleSheet.create({
       fontSize: 18,
       fontWeight: 'bold',
     },
-    listContainer: {
-      width: 250,
-      padding: SIZES.xLarge,
-      backgroundColor: "#FFF",
-      borderRadius: SIZES.medium,
-      justifyContent: "space-between",
-      ...SHADOWS.medium,
-      shadowColor: COLORS.white,
+    cardContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      borderRadius: 80,
+      marginBottom: 10,
+      width: screenWidth - 20,
+      backgroundColor:"#FFF",
+      shadowColor: "#FFF",
     },
     iconContainer: {
       width: 50,
       height: 50,
-      backgroundColor: COLORS.white,
-      borderRadius: SIZES.medium,
-      justifyContent: "center",
-      alignItems: "center",
+      marginRight: 10,
     },
-    logoImage: {
-      width: "70%",
-      height: "70%",
+    iconImage: {
+      width: '100%',
+      height: '100%',
+      resizeMode: 'contain',
+      borderRadius: 70,
     },
-    companyName: {
-      fontSize: SIZES.medium,
-      color: "#B3AEC6",
-      marginTop: SIZES.small / 1.5,
+    textContainer: {
+      flex: 1,
     },
-    infoContainer: {
-      marginTop: SIZES.large,
+    textFormat: {
+      fontSize: 16,
+      // Add other text styles as needed
     },
     flatList: {
-      width: '100%',
+      width: screenWidth - 20,
     },
   });
 
